aebafab24d7ef7629aa07970c73aca75fbd39bdb
Dave <dbayles@gmail.com>
2023-01-26 13:13:59 -0600

    Accordion animation fixes

--- web/themes/isu/src/js/components/accordion.js
+++ web/themes/isu/src/js/components/accordion.js
@@ -5,15 +5,18 @@ toggles.forEach((toggle) => {
   toggle.addEventListener('click', () => {
     toggleContents.forEach((content) => {
       content.classList.remove('active');
+      content.style.height = '0px';
     });
     if (toggle.getAttribute('aria-expanded') === 'true') {
       toggle.setAttribute('aria-expanded', 'false');
+      toggle.parentElement.nextElementSibling.style.height = '0px';
     } else {
       toggles.forEach((button) => {
         button.setAttribute('aria-expanded', 'false');
       });
       toggle.setAttribute('aria-expanded', 'true');
       toggle.parentElement.nextElementSibling.classList.add('active');
+      toggle.parentElement.nextElementSibling.style.height = `${toggle.parentElement.nextElementSibling.scrollHeight}px`;
     }
   });
 });
