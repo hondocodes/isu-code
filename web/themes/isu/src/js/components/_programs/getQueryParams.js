420bbbc73457705418e97728b9c9f620e84b1c56
Dave Bayles <dbayles@gmail.com>
2023-04-13 09:29:06 -0500

    Updated finder query params.

--- web/themes/isu/src/js/components/_programs/getQueryParams.js
+++ web/themes/isu/src/js/components/_programs/getQueryParams.js
@@ -1,45 +1,49 @@
-import 'url-search-params-polyfill'
+import 'url-search-params-polyfill';
 
 export function setQueryParams(filters, search) {
-    const searchParams = new URLSearchParams()
-    searchParams.set('search', search)
+    const searchParams = new URLSearchParams();
+    searchParams.set('search', search);
     Object.keys(filters).forEach((filterName) => {
         if (filterName != 'date') {
-            const filterVals = filters[filterName]
-            searchParams.set(filterName, filterVals.join(';'))
+            const filterVals = filters[filterName];
+            searchParams.set(filterName, filterVals.join(';'));
         }
     })
     const newUrl = decodeURIComponent(
         `${window.location.pathname}?${encodeURIComponent(searchParams)}`
     )
-    window.history.pushState({}, '', newUrl)
+    window.history.pushState({}, '', newUrl);
 }
 
 export function getQueryParams() {
-    const searchParams = new URLSearchParams(window.location.search)
+    const searchParams = new URLSearchParams(window.location.search);
 
-    const preFilterSearch = searchParams.get('search')
-    const preFilterArea = searchParams.get('area')
-
-    const preFilterGrade = searchParams.get('grade')
-    const preFilterDuration = searchParams.get('duration')
+    const preFilterSearch = searchParams.get('search');
+    const preFilterDegree = searchParams.get('degree');
+    const preFilterInterest = searchParams.get('interest');
+    const preFilterCollege = searchParams.get('college');
+    const preFilterLocation = searchParams.get('location');
 
     const preFilterFilters = {}
 
-    if (preFilterArea) {
-        const values = preFilterArea.split(';')
-
-        preFilterFilters.area = values
+    if (preFilterDegree) {
+        const values = preFilterDegree.split(';')
+        preFilterFilters.degree = values;
     }
 
-    if (preFilterGrade) {
-        const values = preFilterGrade.split(';')
-        preFilterFilters.grade = values
+    if (preFilterInterest) {
+        const values = preFilterInterest.split(';')
+        preFilterFilters.interest = values;
     }
 
-    if (preFilterDuration) {
-        const values = preFilterDuration.split(';')
-        preFilterFilters.duration = values
+    if (preFilterCollege) {
+        const values = preFilterCollege.split(';')
+        preFilterFilters.college = values;
+    }
+
+    if (preFilterLocation) {
+        const values = preFilterLocation.split(';')
+        preFilterFilters.location = values;
     }
 
     return { preFilterSearch, preFilterFilters }
